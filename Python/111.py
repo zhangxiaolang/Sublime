b40e66162af3e0ae7e9fe79a7e8da26c0ca1f6c4
zhangxiaolang <zhangxiaolang@users.noreply.github.com>
2018-11-19 11:07:03 +0800

    十一章开始

--- Python/111.py
+++ Python/111.py
@@ -1,12 +1,1 @@
-#10-11
-import json
-a = input('请输入你喜欢的数字：')
-filename = 'number.json'
-
-with open(filename,'w') as f_obj:
-	json.dump(a,f_obj)
-
-
-with open(filename) as f_obj2:
-	a = json.load(f_obj2)
-	print('你喜欢的数字是：{0}'.format(a))
+是一张
