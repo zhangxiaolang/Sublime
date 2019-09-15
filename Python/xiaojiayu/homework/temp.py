de188fd26b114f53f31249404a4229f5168bc80e
zhangxiaolang <zhangxiaolang@users.noreply.github.com>
2019-09-15 18:27:56 +0800

    9.15-2

--- Python/xiaojiayu/homework/temp.py
+++ Python/xiaojiayu/homework/temp.py
@@ -1,2 +1,11 @@
-for i in range(0, 10, 2):
-	print('I Love FishC')
+def fun_narcissus():
+    print('100-999之间的所有水仙花数有如下所示：')
+    num = 100
+    while(num <= 999):
+        a = int(num/100)
+        b = int(num/10) - (a * 10)
+        c = num - (a * 100) - (b * 10)
+        if num == a**3 + b**3 + c**3:
+            print(num,end = ', ')
+        num += 1
+fun_narcissus()
